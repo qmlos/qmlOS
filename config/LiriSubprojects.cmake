bd13c3b7aee6c8ee7a17d49eff80201466db5cd7
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2024-07-10 08:37:52 +0200

    Sort dependencies

--- config/LiriSubprojects.cmake
+++ config/LiriSubprojects.cmake
@@ -4,52 +4,71 @@
 
 # Find all subprojects (i.e. subdirectories with a CMakeLists.txt) and set
 # the variable with the name specified in $result_var with the list of subprojects
-function(_liri_internal_find_subprojects result_var)
+function(_liri_internal_find_subprojects result_var filter_items)
     set(subprojects_list "")
 
-    file(GLOB items LIST_DIRECTORIES true RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}" "*")
-    foreach(item IN LISTS items)
-        if(IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/${item}"
-                AND EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${item}/CMakeLists.txt")
-            list(APPEND subprojects_list "${item}")
-        endif()
-    endforeach()
+    # Find all subprojects (directories with CMakeLists.txt)
+    if(NOT "${filter_items}" STREQUAL "")
+        set(subprojects_list "${filter_items}")
+    else()
+        file(GLOB items LIST_DIRECTORIES true RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}" "*")
+        foreach(item IN LISTS items)
+            if(IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/${item}"
+                    AND EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${item}/CMakeLists.txt")
+                list(APPEND subprojects_list "${item}")
+            endif()
+        endforeach()
+    endif()
 
     message(DEBUG "_liri_internal_find_subprojects: ${subprojects_list}")
 
-    set(${result_var} "${subprojects_list}" PARENT_SCOPE)
-endfunction()
-
-# Sort the list of subprojects in $subprojects based on dependencies found in
-# variables with prefix "__liri_subproject_dependencies_".
-# The sorted list is set in a variable whose name is specified in $out_var.
-function(_liri_internal_sort_subprojects subprojects out_var)
-    set(ordered "")
-
-    foreach(subproject IN LISTS subprojects)
-        # Add subproject to the list if it wasn't already added
-        list(FIND ordered "${subproject}" subproject_index)
-        if(subproject_index EQUAL -1)
-            list(LENGTH ordered subproject_index)
-            list(APPEND ordered "${subproject}")
+    # Collect all subprojects and dependencies
+    set(all_subprojects_and_dependencies ${subprojects_list})
+    foreach(subproject IN LISTS subprojects_list)
+        set(dependencies_var "__liri_subproject_dependencies_${subproject}")
+        if(DEFINED ${dependencies_var})
+            list(APPEND all_subprojects_and_dependencies ${${dependencies_var}})
         endif()
-
-        # Scan the dependencies for this subproject
-        set(dependencies_list_var_name "__liri_subproject_dependencies_${subproject}")
-        foreach(dependency IN LISTS "${dependencies_list_var_name}")
-            # Search the dependency and load it if it wasn't already loaded,
-            # also make sure it is before the subproject that requires it
-            list(FIND ordered "${dependency}" dependency_index)
-            if(dependency_index EQUAL -1)
-                list(INSERT ordered ${subproject_index} "${dependency}")
-            elseif(dependency_index GREATER subproject_index)
-                list(REMOVE_AT ordered ${dependency_index})
-                list(INSERT ordered ${subproject_index} "${dependency}")
-            endif()
-        endforeach()
     endforeach()
 
-    message(DEBUG "_liri_internal_sort_subprojects: ${ordered}")
+    # Remove duplicates
+    list(REMOVE_DUPLICATES all_subprojects_and_dependencies)
 
-    set(${out_var} "${ordered}" PARENT_SCOPE)
+    # Function to order subprojects based on dependencies
+    function(_liri_internal_sort_subprojects subprojects out_var)
+        set(ordered "")
+        foreach(subproject IN LISTS subprojects)
+            # Add subproject to the list if it wasn't already added
+            list(FIND ordered "${subproject}" subproject_index)
+            if(subproject_index EQUAL -1)
+                list(LENGTH ordered subproject_index)
+                list(APPEND ordered "${subproject}")
+            endif()
+
+            # Scan the dependencies for this subproject
+            set(dependencies_list_var_name "__liri_subproject_dependencies_${subproject}")
+            foreach(dependency IN LISTS "${dependencies_list_var_name}")
+                # Search the dependency and load it if it wasn't already loaded,
+                # also make sure it is before the subproject that requires it
+                list(FIND ordered "${dependency}" dependency_index)
+                if(dependency_index EQUAL -1)
+                    list(INSERT ordered ${subproject_index} "${dependency}")
+                elseif(dependency_index GREATER subproject_index)
+                    list(REMOVE_AT ordered ${dependency_index})
+                    list(INSERT ordered ${subproject_index} "${dependency}")
+                endif()
+            endforeach()
+        endforeach()
+
+        message(DEBUG "_liri_internal_sort_subprojects: ${ordered}")
+
+        set(${out_var} "${ordered}" PARENT_SCOPE)
+    endfunction()
+
+    # Sort the subprojects based on dependencies
+    _liri_internal_sort_subprojects("${all_subprojects_and_dependencies}" ordered_subprojects)
+
+    message(DEBUG "_liri_internal_find_subprojects: Final sorted subprojects: ${ordered_subprojects}")
+
+    set(${result_var} "${ordered_subprojects}" PARENT_SCOPE)
 endfunction()
